6b2be8cd27e2f98992ff5c843cfebf536d4881cd
ncharles11 <123888602+ncharles11@users.noreply.github.com>
2025-02-03 09:58:48 +0100

    Add files via upload

---
+++ demo/authentication_backends.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth import get_user_model
+
+UserModel = get_user_model()
+
+class EmailBackend(ModelBackend):
+    """
+    Authenticates against the email field of a User model.
+    """
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            user = UserModel.objects.get(email=username)
+            if user.check_password(password):
+                return user
+        except UserModel.DoesNotExist:
+            return None
